305e3ac70e4a6de06259bc0e963b44c719694312
doron weiss <doron@10fs.co.il>
2019-03-17 15:08:43 +0200

    added range and IsOneOf

---
+++ CSExtensions/GenUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSExtensions {
+  public static class GenUtils {
+    // check if value in list (params[])
+    public static bool IsOneOf<T>(T value, params T[] valuesList) {
+      return valuesList.ToList().Contains(value);
+    }
+  }
+
+
+  // usage:       foreach (typename x in Range.Get( elem1, elem2, elem3,... ))...
+  public static class Range {
+    public static IEnumerable<T> Get<T>(params T[] a) {
+      foreach (var item in a) {
+        yield return item;
+      }
+    }
+
+    public static IEnumerable<object> Get(params object[] a) {
+      foreach (var item in a) {
+        yield return item;
+      }
+    }
+  }
+
+}
